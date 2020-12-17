281574c93b455b90875737d216eb947518c9727a
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-17 11:48:56 +1100

    build a new web form using vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignupForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SignupForm
+    
+  },
+};
 </script>
 
 <style>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background: #eee;
+  margin: 0;
 }
 </style>
